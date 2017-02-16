de56d3047ee1c64bd64302a13203d660f2bcbd8b
Małgorzata Ksionek <meksionek@gmail.com>
2017-02-16 21:17:08 +0100

    Add my bottle class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,36 +1,43 @@
 class Bottles
+  def verse(line)
+    return one_verse if line == 1
+    return last_verse if line == 0
+    "#{line} #{bottle_form(line)} of beer on the wall, " +
+    "#{line} #{bottle_form(line)} of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{line - 1} #{bottle_form(line-1)} of beer on the wall.\n"
+  end
+
+  def verses(start_line, end_line)
+    string = String.new
+    start_line.downto(end_line).each do |line|
+      string << verse(line)
+      string << "\n"
+    end
+    string.chomp
+  end
 
   def song
-    verses(99, 0)
+    verses(99,0)
   end
 
-  def verses(starting, ending)
-    starting.downto(ending).map {|i| verse(i)}.join("\n")
+  private
+
+  def one_verse
+    "1 bottle of beer on the wall, " +
+    "1 bottle of beer.\n" +
+    "Take it down and pass it around, " +
+    "no more bottles of beer on the wall.\n"
   end
 
-  def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number-1} bottles of beer on the wall.\n"
-    end
+  def last_verse
+    "No more bottles of beer on the wall, " +
+    "no more bottles of beer.\n" +
+    "Go to the store and buy some more, " +
+    "99 bottles of beer on the wall.\n"
   end
 
+  def bottle_form(number)
+    number > 1 ? "bottles" : "bottle"
+  end
 end
